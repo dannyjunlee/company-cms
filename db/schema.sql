f68c60901cc73c034a4c88b684761a5e0cbf885e
Danny <dl667@cornell.edu>
2022-10-31 18:55:27 -0400

    created intiial schema sql file with tables setup

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+USE company_db;
+
+CREATE TABLE departments (
+    id INT NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT NOT NULL
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL
+);
