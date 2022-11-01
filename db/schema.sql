b0a0f68eb619d6c4211e95533ac40574468ffdbe
Danny <dl667@cornell.edu>
2022-11-01 10:54:08 -0400

    set up foreign keys in schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,15 +3,20 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE departments (
-    id INT NOT NULL,
-    name VARCHAR(30) NOT NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE roles (
     id INT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
-    department_id INT NOT NULL
+    department_id INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
@@ -20,4 +25,8 @@ CREATE TABLE employees (
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT NOT NULL
+    PRIMARY KEY(id),
+    FOREIGN KEY(role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL
 );
