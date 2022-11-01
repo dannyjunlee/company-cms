9ca2c83908a34027e4cc97066c8141146d2c7ed6
Danny <dl667@cornell.edu>
2022-10-31 22:09:06 -0400

    added comments and image of database schema demo

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+// Dependencies
 const express = require('express');
 const mysql = require('mysql2');
+const cTable = require('console.table');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+// Establish MySQL connection
 const db = mysql.createConnection(
     {
         host: 'localhost',
@@ -16,3 +20,13 @@ const db = mysql.createConnection(
     },
     console.log('Connected to company_db database')
 );
+
+// End if 404
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+// Listen
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
